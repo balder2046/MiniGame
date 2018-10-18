f4ba7c4fe655429dd122fc6720e767a4609a0d49
Balder <void1981@gmail.com>
2018-10-19 02:11:31 +0800

    add objectmanager interface

--- src/ObjectManager.cpp
+++ src/ObjectManager.cpp
@@ -5,3 +5,15 @@ CObjectManager::CObjectManager(){
 CObjectManager::~CObjectManager(){
     
 }
+ CObjectBase *CObjectManager::getObjectByID(long long idFind) const
+ {
+     return 0;
+ }
+void CObjectManager::removeObjectByID(long long idFind)
+{
+
+}
+void CObjectManager::removeObject(CObjectBase *obj)
+{
+    
+}
--- src/ObjectManager.h
+++ src/ObjectManager.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <vector>
+#include "ObjectBase.h"
 class CObjectManager{
 public:
     CObjectManager();
     virtual ~CObjectManager();
+public:
+    CObjectBase *getObjectByID(long long idFind) const;
+    void removeObjectByID(long long idFind);
+    void removeObject(CObjectBase *obj);
+
+    
 private:
     
 
